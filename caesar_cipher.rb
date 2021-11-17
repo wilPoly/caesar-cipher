1c7654b4c89d421d8205278bf82c8f29a166b595
wilpoly <polype@pm.me>
2021-11-17 18:19:53 +0100

    working on it...

---
+++ caesar_cipher.rb
@@ -0,0 +1,12 @@
+def caesar_cipher(string, shift)
+  # on recupère la valeur du shift et on génère un array avec les lettres codées
+    # on génère un array (a..z).to_a
+    base = ("a".."z").to_a
+
+    # on map l'array en partant de l'index égal à shift
+    
+    # les 
+  # on convertit la string en array et on la split
+  # pour chaque caractère, si elle est compris entre a-z ou A-Z
+  # 
+end

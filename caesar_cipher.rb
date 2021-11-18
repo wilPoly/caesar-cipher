a1f000f21095c9761610ed2e3aeb1b9e20a7beb9
wilpoly <polype@pm.me>
2021-11-18 21:12:00 +0100

    Add shifting but needs to take into account the case

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,12 +1,27 @@
+require "pry-byebug"
+
 def caesar_cipher(string, shift)
   # on recupère la valeur du shift et on génère un array avec les lettres codées
     # on génère un array (a..z).to_a
-    base = ("a".."z").to_a
-
+    alpha = ("a".."z").to_a
     # on map l'array en partant de l'index égal à shift
-    
+    alpha_shifted = alpha.slice(0...shift)
+    cypher = alpha.slice(shift..alpha.length-1) + alpha_shifted
+    binding.pry
     # les 
   # on convertit la string en array et on la split
+  string_array = string.split("")
+  new_string = []
   # pour chaque caractère, si elle est compris entre a-z ou A-Z
-  # 
+  string_array.each.with_index do |letter, index|
+    if alpha.include?(letter.downcase)
+      new_string << cypher[alpha.index(letter.downcase)]
+    else
+      new_string << letter
+    end
+  end
+  puts new_string.join("")
+
 end
+
+caesar_cipher("What a string!", 5)
